5fe5d8229bf9dc5479b580f14ae846bf2a85556e
Deniz Erisen <denizerisen@windowslive.com>
2020-03-02 14:59:39 +0300

    Revert "mapping from json, deleting,basic inputs"
    This reverts commit 1b75a54f1be70557e96637d7cc509ada5a83f5ac.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./css/main.css";
-import Meals from "./components/meals/Meals";
+import React from 'react';
+import './css/main.css';
+import Meals from './components/meals/Meals'
+import IngredientInput from './components/form/IngredientInput'
 
 function App() {
   return (
     <div className="App grid">
-      <Meals />
-
-      <div className="column__nutrition"></div>
+      <div className="column__meal">
+        <Meals/>
+      </div>
+      <div className="column__nutrition">
+      <IngredientInput/>
+      </div>
     </div>
   );
 }
