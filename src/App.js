96c19560fb964e6ee5bcc609105dac5087766273
Deniz Erisen <denizerisen@windowslive.com>
2020-03-02 14:59:47 +0300

    Revert "started on input view"
    This reverts commit 184bc34b56cb98b665ec2114d95aa85c19effe43.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './css/main.css';
 import Meals from './components/meals/Meals'
-import IngredientInput from './components/form/IngredientInput'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Meals/>
       </div>
       <div className="column__nutrition">
-      <IngredientInput/>
       </div>
     </div>
   );
